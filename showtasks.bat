11fc31d994cdbc013c2606b0050fe6f7d7eff14c
Your Name <Cacek@users.noreply.github.com>
2017-12-03 01:22:19 +0100

    runcrud fixes

--- showtasks.bat
+++ showtasks.bat
@@ -4,10 +4,11 @@ echo.
 echo GRADLEW BUILD has errors - breaking work
 goto fail
 
-:fail
-echo.
-echo There were errors
-
 :start
 choice /d y /t 3 > nul
 start chrome --new-window http://localhost:8080/crud/v1/task/getTasks
+@exit /B
+
+:fail
+echo.
+echo There were errors
